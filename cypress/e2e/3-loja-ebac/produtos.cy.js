9c261c2c38fb79d936f6b063e17c75d9f213e317
dante <dantefranzoi@gmail.com>
2024-02-21 19:10:19 -0300

    massa de dados em lista

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -12,17 +12,33 @@ describe('Funcionalidade: Produtos', () => {
                 cy.get('#tab-title-description > a').should('contain', 'Descrição')
     });
 
-
     it('Deve buscar um produto com sucesso', () => {
         produtosPages.buscarProdutos('Arcadio Gym Short')
         cy.get('.product_title').should('contain','Arcadio Gym Short')
     });
 
     it('Deve visitar a página do produto', () => {
-        
+        produtosPages.visitarProduto('Arcadio-Gym-Short')
+        cy.get('.product_title').should('contain','Arcadio Gym Short')
     });
 
     it('Deve adicionar produto ao carrinho', () => {
-        
+        let qtd = 7
+        produtosPages.buscarProdutos('Aero Daily Fitness Tee')
+        produtosPages.addProdutoCarrinho('M', 'Brown', qtd)
+        cy.get('.woocommerce-message').should('contain', qtd + ' × “Aero Daily Fitness Tee” foram adicionados no seu carrinho.')
     });
+
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dadoscy', () => {
+        cy.fixture('produtos').then(dados => {
+            produtosPages.buscarProdutos(dados[1].nomeProduto)
+            produtosPages.addProdutoCarrinho(
+                dados[1].tamanho, 
+                dados[1].cor, 
+                dados[1].quantidade)
+            cy.get('.woocommerce-message').should('contain', dados[1].nomeProduto)
+        })
+        });
+        
+        
 });
