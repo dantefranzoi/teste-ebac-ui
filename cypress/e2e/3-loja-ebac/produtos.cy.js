1bc49b64c6002966ce32a1493dff68555d504b9d
dante <dantefranzoi@gmail.com>
2024-02-20 22:32:58 -0300

    page objects

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,18 +1,28 @@
 /// <reference types="cypress"/>
+import produtosPages from "../../support/page-objects/produtos.pages";
 
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPages.visitarUrl()
     });
     
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.products > .row')
-            //.first()
-            //.last()
-            //.eq(2)
-            .contains('Aether Gym Pant')
-            .click()
-            cy.get('#tab-title-description > a').should('contain', 'Descrição')
+            produtosPages.buscarProdutoLista('Aero Daily Fitness Tee')
+                cy.get('#tab-title-description > a').should('contain', 'Descrição')
+    });
+
+
+    it('Deve buscar um produto com sucesso', () => {
+        produtosPages.buscarProdutos('Arcadio Gym Short')
+        cy.get('.product_title').should('contain','Arcadio Gym Short')
+    });
+
+    it('Deve visitar a página do produto', () => {
+        
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
+        
     });
 });
